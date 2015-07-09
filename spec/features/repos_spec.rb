629188c01f55585e37a2deba60611c1576503662
changamanda <amanda.chang10@gmail.com>
2015-07-09 10:21:07 -0400

    Remove first test

--- spec/features/repos_spec.rb
+++ spec/features/repos_spec.rb
@@ -1,11 +1,6 @@
 require_relative '../spec_helper'
 
 describe "authentication" do
-  it "succeeds, using the Github API" do
-    visit '/auth?code=20'
-    expect(page.status_code).to eq 200
-  end
-
   it "displays the username on the page" do
     visit '/auth?code=20'
     expect(page).to have_content 'your_username'
